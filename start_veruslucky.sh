8fbbff341a3f3f7fd80c54aaf2422f648b304db2
irulwae <99885782+irulwae@users.noreply.github.com>
2022-02-17 20:07:45 +0700

    Add files via upload

---
+++ start_veruslucky.sh
@@ -0,0 +1,5 @@
+
+#!/bin/sh
+reset
+
+./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://ap.luckpool.net:3956 --wallet RKYzGPydWAVkniGavVPR6om7ESFq5t5CTE.admin1 --password irul --cpu-threads 2 
